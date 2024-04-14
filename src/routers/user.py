b8b1974a1e5c816d3cb73ac23f136f1cd6e0c7ad
Manoel Carvalho <manoelhc@gmail.com>
2024-04-14 17:39:09 -0300

    fix: add fixes

--- src/routers/user.py
+++ src/routers/user.py
@@ -1,13 +1,33 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request, Response
+from fastapi.routing import APIRoute
 from sqlalchemy.exc import IntegrityError
 from models.user import User, UserSimple, UserCreate, UserUpdate
 from sqlmodel import Session, create_engine, select
 import config
 from datetime import datetime
-
+from collections.abc import Callable
 
 engine = create_engine(config.DATABASE_URL, echo=True)
-router = APIRouter()
+
+
+class UserRoute(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def custom_route_handler(request: Request) -> Response:
+            response: Response = await original_route_handler(request)
+            response.headers["Content-Type"] = "application/json"
+            response.headers["Sec-Fetch-Dest"] = "json"
+            response.headers["Sec-Fetch-Site"] = "same-origin"
+            response.headers["Sec-Fetch-Mode"] = "cors"
+            return response
+
+        return custom_route_handler
+
+
+router = APIRouter(route_class=UserRoute)
+
+# Adding default headers for the API
 
 ERROR_USER_NOT_CHANGED = HTTPException(
     status_code=400,
